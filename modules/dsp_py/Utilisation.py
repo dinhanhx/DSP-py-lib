57174109cec29a2297de46b740fd9f8bd5a8bd60
dinhanhx <dinhanhx@gmail.com>
2020-04-13 15:20:45 +0700

    Add Fill() and PrintSignal() to Utilisation.py file

---
+++ modules/dsp_py/Utilisation.py
@@ -0,0 +1,50 @@
+import numpy as np
+from colorama import init
+init() # This is for Windows
+from colorama import Fore
+
+def Fill(x, n):
+    # Add meaningful zeros to x
+    # Also expand n till n == 0
+    x_type = x.dtype
+    n_type = n.dtype
+    if 0 in n:
+        return x, n
+    else:
+        if np.amax(n) > 0:
+            subn = np.arange(0, n[0])
+            n = np.concatenate((subn, n), axis = None)
+            subx = np.zeros(subn.size)
+            x = np.concatenate((subx, x), axis = None)
+        else:
+            subn = np.arange(n[-1]+1, 1)
+            n = np.concatenate((n, subn), axis = None)
+            subx = np.zeros(subn.size)
+            x = np.concatenate((x, subx), axis = None)
+
+    return x.astype(x_type), n.astype(n_type)
+
+def PrintSignal(x, n):
+    x, n = Fill(x, n) # make sure the signal has meaningful zeros
+    # Print x and x(0) is colored green
+    zero_pos = int(np.where(n == 0)[0])
+    s = '['
+    for i in range(x.size):
+        if i == zero_pos:
+            s = s + Fore.GREEN + str(x[i]) + Fore.WHITE + ' '
+        else:
+            s = s + str(x[i]) + ' '
+
+
+    s = s[:-1]
+    s = s + ']'
+    print(s)
+    return ''
+
+
+# x = np.array([1, 1, 1])
+# n = np.array([2, 3, 4])
+#
+# PrintSignal(x, n)
+#
+# x, n = Fill(x, n)
